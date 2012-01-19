d166d1f3b898da8def6ee920c4ba6eb0f7a3e563
R1ckye <casillas.real.madrid.spain@gmail.com>
2012-01-19 13:21:44 +0100

    first commit

---
+++ lib/siriproxy-thatswhatshesaid.rb
@@ -0,0 +1,37 @@
+require 'cora'
+require 'siri_objects'
+require 'open-uri'
+require 'url_escape'
+
+class SiriProxy::Plugin::ThatsWhatSheSaid < SiriProxy::Plugin
+  def initialize(config)
+    #if you have custom configuration options, process them here!
+  end
+
+  listen_for /iphone/i do
+  response = say "An iPhone Blog"
+  request_completed
+  end
+  
+  def process(text) 
+
+     url = "http://twss-classifier.heroku.com/?sentence="
+     url << URLEscape.escape(text)
+     response = ""
+     open(url) {
+        |f|
+        response = f.read
+     }
+     if( response =~ /that's what she said!/i)
+        val = true 
+     else
+        val = false
+     end
+     
+     if(val)
+        say "That's what she said!"
+        request_completed 
+     end 
+     return val 
+  end
+end
